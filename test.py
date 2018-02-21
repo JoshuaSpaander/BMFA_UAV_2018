081f8ee38ddeed3547fc77a499c473bd6d9e8461
midasgossye <midasgossye@hotmail.com>
2018-02-21 20:54:53 +0100

    First test

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 20:45:25 2018
+
+@author: midas
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+file_name = "test.txt"
+np.genfromtxt(file_name)
